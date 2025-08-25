a2c5e847edb093cd517a2b557cbf204eb4c7b692
Dokiemon <131017111+Dokiemon@users.noreply.github.com>
2025-08-25 20:03:33 +0000

    ela me olha e vê um banco

--- ROOT/app.js
+++ ROOT/app.js
@@ -1,8 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const App = express();
 const PORT = 3000;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Banco conectado");
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+connectDB();
 
 App.get('/', (req , res) => {
     res.send("ALGUEM CHAMA O CAPITÃO");
